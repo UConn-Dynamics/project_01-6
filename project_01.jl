316fc6319966c2a1236d01311ad502ac106ab2b6
Ryan C. Cooper <ryan.c.cooper@uconn.edu>
2026-02-10 15:46:43 -0500

    creating project_01

---
+++ project_01.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f17103ea-06bf-11f1-a2b0-79e68ed152eb
+md"""![Spinning pendulum with rotating  and fixed coordinate systems.](./spinning_pendulum.png)"""
+
+# ╔═╡ 0d9be664-d7c5-4084-add2-25e5418742d6
+ls
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═f17103ea-06bf-11f1-a2b0-79e68ed152eb
+# ╠═0d9be664-d7c5-4084-add2-25e5418742d6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
